28bd4eb8672f26d7e614ef22d4f98540ccccf5f2
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-05-27 22:50:46 +0200

    clean up mui-button.ts

--- common/theme/components/mui-button.ts
+++ common/theme/components/mui-button.ts
@@ -3,9 +3,6 @@ import { themeGlobals } from "../theme.globals";
 
 export const MuiButton: Components<Theme>["MuiButton"] = {
   styleOverrides: {
-    outlined: {
-      background: "#eaf4dc",
-    },
     root: {
       textTransform: "none",
       padding: ".8rem",
