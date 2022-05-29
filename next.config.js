e0c94d7c3f5afc05507811430c9017c909da001c
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-05-29 22:03:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,20 @@
-const modulesForCompile = ['@capacitor-community/storage-react']
-const withTM = require('next-transpile-modules')(modulesForCompile);
-const withPlugins = require('next-compose-plugins');
+const modulesForCompile = ["@capacitor-community/storage-react"];
+const withTM = require("next-transpile-modules")(modulesForCompile);
+const withPlugins = require("next-compose-plugins");
 
 const nextConfig = {
   reactStrictMode: true,
+  image: {
+    domains: "https://mukosoft-app.vercel.app",
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
+      use: ["@svgr/webpack"],
     });
 
     return config;
-  }
-}
+  },
+};
 
-module.exports =withPlugins([withTM], nextConfig)
+module.exports = withPlugins([withTM], nextConfig);
