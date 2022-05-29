aae915fa1cb04c619ed718d0853154e7eed2637d
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-05-29 21:59:53 +0200

    use next/image instead of img

--- packages/mukosoft-cookbook/components/recipeList/RecipeList.tsx
+++ packages/mukosoft-cookbook/components/recipeList/RecipeList.tsx
@@ -1,7 +1,8 @@
 import { ImageList, ImageListItem } from "@mui/material";
 import React from "react";
-import { Recipe } from "@models/recipe";
+import { Recipe } from "../../models/recipe";
 import { Box } from "@mui/system";
+import Image from "next/image";
 
 interface RecipeListProps {
   recipes?: Recipe[];
@@ -20,9 +21,8 @@ export default function RecipeList({
             key={recipe.title}
             className="shadow-md rounded-md overflow-hidden"
           >
-            <img
+            <Image
               src={`${recipe.image}?fit=crop&auto=format`}
-              srcSet={`${recipe.image}?auto=format&dpr=2 2x`}
               alt={recipe.title}
               loading="lazy"
             />
