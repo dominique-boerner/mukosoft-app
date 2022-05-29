a62013841475b7f70af48092438a8282e34f9484
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-05-29 13:15:22 +0200

    Update Intro.tsx

--- modules/intro/Intro.tsx
+++ modules/intro/Intro.tsx
@@ -59,7 +59,7 @@ export default function Intro() {
 
   return (
     <Box
-      className="flex flex-col fixed top-0 justify-evenly align-center h-screen"
+      className="flex flex-col fixed top-0 justify-evenly align-center h-screen w-screen"
       style={{ background: activeTheme.palette.primary.main }}
     >
       <Box className="flex flex-col h-screen justify-evenly mx-6">
