c6f31fd2781ac7814bbc0c07719b3de90c5fa74f
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-08-13 21:17:13 +0200

    more cleanups

--- front-end/src/app/features/settings/settings.page.ts
+++ front-end/src/app/features/settings/settings.page.ts
@@ -6,18 +6,12 @@ import { SettingsService } from './services/settings-service';
   templateUrl: 'settings.page.html',
 })
 export class SettingsPage {
-  private readonly initialName = this.settingsService.getPatientName();
-  private readonly initialBirthdate = null;
-
   readonly nameLabel = 'name';
   readonly birthdateLabel = 'birthdate';
 
   name = this.settingsService.getPatientName();
   profileImage = this.settingsService.getPatientAvatar();
-
   birthDate = null;
-
-  hasChangedSomething = false;
 
   constructor(private readonly settingsService: SettingsService) {}
 }
