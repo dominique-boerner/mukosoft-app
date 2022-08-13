1309828ac28e90ae7526ab906bee60bacffbe070
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-08-13 20:55:46 +0200

    refactor AppInitializerFactory

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, Injectable, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -12,16 +12,14 @@ import {
 } from '@ngx-translate/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { Store, StoreModule } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { medicationRequestReducer } from './core/reducer/medication-request.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
-import { PatientService } from './core/services/patient-service/patient.service';
-import { Patient } from 'fhir/r4';
-import { setPatient } from './core/actions/patient.action';
-import { PatientDatabaseService } from './core/services/patient-database-service/patient-database.service';
 import { patientReducer } from './core/reducer/patient.reducer';
+import { AppInitializerProvider } from './core/provider/app-initializer.provider';
+import { AppInitializerService } from './features/medication/services/app-initializer-service/app-initializer.service';
 
 export const createTranslateLoader = (http: HttpClient) =>
   new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -30,29 +28,6 @@ const appState = {
   medicationRequests: medicationRequestReducer,
   patient: patientReducer,
 };
-
-@Injectable()
-export class AppInitializer {
-  constructor(
-    private readonly patientService: PatientService,
-    private readonly store: Store<Patient>,
-    private readonly patientDatabaseService: PatientDatabaseService
-  ) {}
-
-  resolve() {
-    this.patientDatabaseService.getPatient().then((response) => {
-      const noPatientDataExists = response.total_rows === 0;
-      if (noPatientDataExists) {
-        const newPatient = this.patientService.getInitialPatient();
-        this.patientDatabaseService.putPatient(newPatient).catch((err) => err);
-        this.store.dispatch(setPatient({ patient: newPatient }));
-      } else {
-        const patient = response.rows[0].doc;
-        this.store.dispatch(setPatient({ patient }));
-      }
-    });
-  }
-}
 
 @NgModule({
   declarations: [AppComponent],
@@ -78,16 +53,9 @@ export class AppInitializer {
     }),
   ],
   providers: [
-    AppInitializer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: (service: AppInitializer) => {
-        return () => service.resolve();
-      },
-      deps: [AppInitializer],
-      multi: true,
-    },
+    AppInitializerService,
+    AppInitializerProvider,
     TranslateStore,
   ],
   bootstrap: [AppComponent],
