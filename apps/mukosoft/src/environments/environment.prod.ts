fdbf6ed88c47208cdde8a690822e44aaf70d7b66
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-09-04 13:51:55 +0200

    add interface for environment files

--- apps/mukosoft/src/environments/environment.prod.ts
+++ apps/mukosoft/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
-export const environment = {
+import { IEnvironment } from "./environment.interface";
+
+export const environment: IEnvironment = {
   production: true,
   defaultLanguage: "de",
 };
--- apps/mukosoft/src/environments/environment.ts
+++ apps/mukosoft/src/environments/environment.ts
@@ -2,9 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+import { IEnvironment } from "./environment.interface";
+
+export const environment: IEnvironment = {
   production: false,
-  defaultLanguage: 'de',
+  defaultLanguage: "de",
 };
 
 /*
