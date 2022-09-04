e204814705248cd1dc2537febca13411460547cb
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-09-04 13:28:06 +0200

    add code coverage

--- apps/mukosoft/jest.config.ts
+++ apps/mukosoft/jest.config.ts
@@ -1,22 +1,24 @@
 /* eslint-disable */
 export default {
-  displayName: 'mukosoft',
-  preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  displayName: "mukosoft",
+  preset: "../../jest.preset.js",
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [".component.ts"],
+  setupFilesAfterEnv: ["<rootDir>/src/test-setup.ts"],
   globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+      stringifyContentPathRegex: "\\.(html|svg)$",
     },
   },
-  coverageDirectory: '../../coverage/apps/mukosoft',
+  coverageDirectory: "../../coverage/apps/mukosoft",
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    "^.+\\.(ts|mjs|js|html)$": "jest-preset-angular",
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
   snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
+    "jest-preset-angular/build/serializers/no-ng-attributes",
+    "jest-preset-angular/build/serializers/ng-snapshot",
+    "jest-preset-angular/build/serializers/html-comment",
   ],
 };
