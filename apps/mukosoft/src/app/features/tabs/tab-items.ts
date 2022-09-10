9f4dbd396bb474e18bfc44108fb84e5c834f4b8f
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-09-10 11:39:10 +0200

    - remove cookbook icon in navigation - use MukoSoftIcons instead of hard coded strings

--- apps/mukosoft/src/app/features/tabs/tab-items.ts
+++ apps/mukosoft/src/app/features/tabs/tab-items.ts
@@ -1,28 +1,29 @@
 import { Tab } from "./models/tab";
+import { MukoSoftIcons } from "../../../theme/icons";
 
 export const tabItems: Tab[] = [
   {
     path: "home",
     redirectTo: "home",
     label: "navigation.tabs.home",
-    icon: "fi fi-rr-home",
+    icon: MukoSoftIcons.home,
   },
   {
     path: "medications",
     redirectTo: "medications",
     label: "navigation.tabs.medications",
-    icon: "fi fi-rr-medicine",
+    icon: MukoSoftIcons.medication,
   },
-  {
-    path: "cookbook",
-    redirectTo: "cookbook",
-    label: "navigation.tabs.cookbook",
-    icon: "fi fi-rr-carrot",
-  },
+  // {
+  //   path: "cookbook",
+  //   redirectTo: "cookbook",
+  //   label: "navigation.tabs.cookbook",
+  //   icon: "fi fi-rr-carrot",
+  // },
   {
     path: "settings",
     redirectTo: "settings",
     label: "navigation.tabs.settings",
-    icon: "fi fi-rr-settings-sliders",
+    icon: MukoSoftIcons.settings,
   },
 ];
--- apps/mukosoft/src/theme/icons.ts
+++ apps/mukosoft/src/theme/icons.ts
@@ -1,7 +1,14 @@
-export type IconFeature = "medication" | "deleteMedication" | "add";
+export type IconFeature =
+  | "medication"
+  | "deleteMedication"
+  | "add"
+  | "settings"
+  | "home";
 
 export const MukoSoftIcons: Record<IconFeature, string> = {
   add: "fi fi-rr-plus",
   medication: "fi fi-rr-medicine",
   deleteMedication: "fi fi-rr-trash",
+  home: "fi fi-rr-home",
+  settings: "fi fi-rr-settings-sliders",
 };
