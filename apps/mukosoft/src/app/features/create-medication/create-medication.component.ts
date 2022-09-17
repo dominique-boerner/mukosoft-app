f90cda54740fffe4db64f9a3207bb655fc6b67f6
Dominique Börner <69742611+mukosoft@users.noreply.github.com>
2022-09-17 18:24:26 +0200

    local notification testing

--- apps/mukosoft/src/app/features/create-medication/create-medication.component.ts
+++ apps/mukosoft/src/app/features/create-medication/create-medication.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormControl, Validators } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Logger } from "../../core/util/logger/logger";
 import { medicationFormCodings } from "../../core/medication-form-codings";
 import { Coding } from "fhir/r4";
@@ -10,37 +10,38 @@ import { LocalNotifications } from "@awesome-cordova-plugins/local-notifications
   templateUrl: "./create-medication.component.html",
 })
 export class CreateMedicationComponent {
-  readonly medicationFormCodings = medicationFormCodings;
-  name = new FormControl("", [Validators.required, Validators.nullValidator]);
-  amount = new FormControl(1, [
-    Validators.required,
-    Validators.min(1),
-    Validators.pattern("^[0-9]*$"),
-  ]);
-  medicationForm = new FormControl<Coding>(medicationFormCodings[0], [
-    Validators.required,
-  ]);
-  times = new FormControl<Date[]>([new Date()], [Validators.required]);
+  public readonly medicationFormCodings = medicationFormCodings;
+  public formGroup = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.nullValidator]),
+    amount: new FormControl(1, [
+      Validators.required,
+      Validators.min(1),
+      Validators.pattern("^[0-9]*$"),
+    ]),
+    medicationForm: new FormControl<Coding>(medicationFormCodings[0], [
+      Validators.required,
+    ]),
+    times: new FormControl<Date[]>([new Date()], [Validators.required]),
+  });
 
-  constructor(private localNotifications: LocalNotifications) {}
+  constructor(private readonly localNotifications: LocalNotifications) {}
 
-  saveClick() {
-    const medicationName = this.name.value;
-    const amount = this.amount.value;
-    const times = this.times.value;
-    Logger.info(`Medication ${amount}x ${medicationName}, Times: ${times}`);
+  public saveClick() {
+    const formGroupControls = this.formGroup.controls;
+    const medicationName = formGroupControls.name.value;
+    const amount = formGroupControls.amount.value;
+    const times = formGroupControls.times.value;
+    if (!this.formGroup.touched || this.formGroup.dirty) {
+      Logger.info(`Isdirty`);
+    } else {
+      Logger.info(`Medication ${amount}x ${medicationName}, Times: ${times}`);
+    }
     this.localNotifications.schedule({
       id: 1,
-      trigger: { at: new Date(new Date().getTime() + 5) },
       text: "Single ILocalNotification",
       data: {
         secret: `Medication ${amount}x ${medicationName}, Times: ${times}`,
       },
     });
-  }
-
-  onDateTimeSelect(date: Date) {
-    this.times.setValue([...this.times.value, date]);
-    console.log(date);
   }
 }
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.mukosoft.app",
+  appName: "mukosoft",
+  webDir: "dist/apps/mukosoft",
+  bundledWebRuntime: false,
+};
+
+export default config;
